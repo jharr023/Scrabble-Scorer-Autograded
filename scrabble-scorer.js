0b064a01345d55b401ca0ca32b7182f81d274cae
jharr023 <jharringtonkc@gmail.com>
2024-02-14 17:04:33 -0600

    commit

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -13,21 +13,15 @@ const oldPointStructure = {
 };
 
 const scoringAlgorithms = [
-  {
-    name: 'Simple Score',
+  { name: 'Simple Score',
     description: 'Each letter is worth 1 point.',
-    scoringFunction: simpleScorer,
-  },
-  {
-    name: 'Bonus Vowels',
+    scoringFunction: simpleScorer, },
+  { name: 'Bonus Vowels',
     description: 'Vowels are 3 pts, consonants are 1 pt.',
-    scoringFunction: vowelBonusScorer,
-  },
-  {
-    name: 'Scrabble',
+    scoringFunction: vowelBonusScorer, },
+  { name: 'Scrabble',
     description: 'The traditional scoring algorithm.',
-    scoringFunction: scrabbleScorer
-  }
+    scoringFunction: scrabbleScorer }
 ]; 
 console.log("Number of scoring algorithms:", scoringAlgorithms.length);
 
